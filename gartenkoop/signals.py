32bf76348d596074505dae7055f392b7bf308dc7
pascalfree <pascal.free@ihdg.ch>
2023-06-11 23:25:38 +0200

    notify superusers on depot change

--- gartenkoop/signals.py
+++ gartenkoop/signals.py
@@ -1,9 +1,8 @@
+from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.template.loader import get_template
 from juntagrico.mailer import EmailSender, organisation_subject, base_dict
 from juntagrico.signals import depot_changed
-
-from gartenkoop.settings import ADMINS
 
 
 @receiver(depot_changed)
@@ -11,5 +10,5 @@ def on_depot_change(sender, **kwargs):
     EmailSender.get_sender(
         organisation_subject('Ein Mitglied hat das Depot geändert'),
         get_template('juntagrico/mails/admin/depot_changed.txt').render(base_dict(kwargs)),
-        bcc=[ADMINS[0][1]]
+        bcc=User.objects.filter(is_superuser=True).values_list('member__email', flat=True)
     ).send()
