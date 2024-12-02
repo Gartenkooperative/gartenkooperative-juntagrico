92bb71822c0b470795210604a8fa8a1039970404
pascalfree <pascal.free@ihdg.ch>
2024-12-02 08:41:15 +0100

    remove custom notification that is not part of juntagrico

--- gartenkoop/apps.py
+++ gartenkoop/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class GartenkoopConfig(AppConfig):
     name = 'gartenkoop'
     verbose_name = "gartenkooperative"
-
-    def ready(self):
-        from . import signals  # noqa: F401
